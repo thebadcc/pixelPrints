510f8ed2150a22fe78d080d9d2201c0847572bff
thebadcc <thebadcc@outlook.com>
2021-01-29 17:20:25 -0500

    Update ERC721.sol

--- solidity/ERC721.sol
+++ solidity/ERC721.sol
@@ -18,7 +18,6 @@ contract pixelPrints is ERC721 {
    
     address owner = msg.sender;
     string public termsURI = "";
-    string public version = "0.1";
     
    
     modifier onlyOwner {
@@ -31,30 +30,40 @@ contract pixelPrints is ERC721 {
     function mintToken (address _owner, uint _tokenID, string memory _tokenURI) public onlyOwner {
         _mint(_owner, _tokenID);
         _setTokenURI(_tokenID, _tokenURI);
+        
+        emit eMintToken(_owner, _tokenID, _tokenURI);
     }
  
     function updateTokenURI (uint _tokenID, string memory _tokenURI) public onlyOwner {
         _setTokenURI(_tokenID, _tokenURI);
+        emit eUpdateTokenURI(_tokenID, _tokenURI);
     }
     
     function burnToken (uint _tokenID) public onlyOwner {
         _burn(_tokenID);
+        emit eBurnToken(_tokenID);
     }
     
     function transferOwnership (address _owner) public onlyOwner {
         owner = _owner;
+        emit eTransferOwnership(msg.sender, _owner);
     }
     
     function updateTermsURI (string memory _termsURI) public onlyOwner{
         termsURI = _termsURI;
-    }
-    
-    function updateVersion (string memory _version) public onlyOwner{
-        version = _version;
+        emit eUpdateTokenURI(_termsURI);
     }
     
     function setBaseURI (string memory _baseURI) public onlyOwner{
         _setBaseURI(_baseURI);
+        eSetBaseURI(_baseURI);
     }
     
+    event eMintToken(address _owner, uint _tokenID, string _tokenURI);
+    event eUpdateTokenURI(uint _tokenID, string _tokenURI);
+    event eBurnToken(uint _tokenID);
+    event eTransferOwnership(address _owner, address _newOwner);
+    event eUpdateTokenURI(string _termsURI);
+    event eSetBaseURI(string _baseURI);
+
 }
